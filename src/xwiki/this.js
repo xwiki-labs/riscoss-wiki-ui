3cb868dff2d3f1adc6eaf3501edad69e13ae55a2
Caleb James DeLisle <cjd@cjdns.fr>
2014-10-15 10:34:24 +0200

    initial state, before divergence

---
+++ src/xwiki/this.js
@@ -0,0 +1,6 @@
+Package(function (pack, XWiki) {
+    pack.setName("RISCOSS Domain Manager Wiki");
+    pack.setDescription("RISCOSS Platform - Domain Manager - UI - Wiki");
+    pack.setExtensionId("eu.riscoss:riscoss-platform-dm-ui-wiki");
+    pack.setXarName("riscoss-platform-dm-ui-wiki-0.20-SNAPSHOT.xar")
+});

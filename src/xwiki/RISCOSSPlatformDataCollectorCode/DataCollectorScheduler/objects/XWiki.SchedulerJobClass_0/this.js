c63e6564ccb1022372f205e2813e624adbe0071d
Caleb James DeLisle <cjd@cjdns.fr>
2014-12-23 18:01:05 +0100

    only run once every 5 minutes + minor fixes to the evaluator

--- src/xwiki/RISCOSSPlatformDataCollectorCode/DataCollectorScheduler/objects/XWiki.SchedulerJobClass_0/this.js
+++ src/xwiki/RISCOSSPlatformDataCollectorCode/DataCollectorScheduler/objects/XWiki.SchedulerJobClass_0/this.js
@@ -1,7 +1,7 @@
 XWikiObj(function (obj) {
     obj.setContextLang("en");
     obj.setContextUser("xwiki:XWiki.Admin");
-    obj.setCron("0 * * * * ?");
+    obj.setCron("0 0/5 * * * ?");
     obj.setJobClass("com.xpn.xwiki.plugin.scheduler.GroovyJob");
     obj.setJobDescription("The schedule which is used to invoke data collectors for all RISCOSS entities.");
     obj.setJobName("Data Collector Schedule");

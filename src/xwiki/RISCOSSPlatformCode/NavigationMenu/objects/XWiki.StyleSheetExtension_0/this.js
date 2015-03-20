b28357b1af620f87fec9cc8156cc84293f58b9a9
Caleb James DeLisle <cjd@cjdns.fr>
2015-03-20 16:16:32 +0100

    Add optional buttons for user feedback and for expert review

---
+++ src/xwiki/RISCOSSPlatformCode/NavigationMenu/objects/XWiki.StyleSheetExtension_0/this.js
@@ -0,0 +1,7 @@
+XWikiObj(function (obj) {
+    obj.setCache("long");
+    obj.setCode(".riscoss-feedback{\n    background-image: url(\"$doc.getAttachmentURL('feedback.png')\");\n    background-position: right center;\n    background-repeat: no-repeat;\n    padding-right: 20px;\n}");
+    obj.setName("");
+    obj.setParse("1");
+    obj.setUse("always");
+});

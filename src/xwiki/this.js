27d9473be7b19895deb8b8e4e08466de03cfa145
Caleb James DeLisle <cjd@cjdns.fr>
2015-04-01 16:23:59 +0200

    replace this.js with old version

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -1,6 +1,6 @@
 Package(function (pack, XWiki) {
-    pack.setName("Nameless");
-    pack.setDescription("none");
-    pack.setExtensionId("org.xwiki:unnamed-extension");
-    pack.setXarName("i-forgot-to-edit-src-slash-xwiki-slash-this-dot-js.xar");
+    pack.setName("RISCOSS Data Collector Code");
+    pack.setDescription("RISCOSS Platform - Domain Manager - UI - Data Collector Code");
+    pack.setExtensionId("eu.riscoss:riscoss-platform-dm-ui");
+    pack.setXarName("riscoss-platform-dm-ui-0.24-SNAPSHOT.xar");
 });

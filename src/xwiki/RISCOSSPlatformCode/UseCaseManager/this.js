ecba0c14930f28f8ba2ad368aa27eec025bc6afc
mouhb <mohamed.boussa@xwiki.com>
2015-04-01 17:26:00 +0200

    Allows seeing the components evaluated with a usecase when you are not logged in

---
+++ src/xwiki/RISCOSSPlatformCode/UseCaseManager/this.js
@@ -0,0 +1,13 @@
+XWikiDoc(function (doc) {
+    doc.setAuthor("xwiki:XWiki.superadmin");
+    doc.setCustomClass(undefined);
+    doc.setContentAuthor("xwiki:XWiki.superadmin");
+    doc.setCreationDate("1427901028000");
+    doc.setDate("1427901189000");
+    doc.setContentUpdateDate("1427901189000");
+    doc.setVersion("1.3");
+    doc.setDefaultTemplate(undefined);
+    doc.setValidationScript(undefined);
+    doc.setComment("Imported from XAR");
+    doc.setMinorEdit("true");
+});
